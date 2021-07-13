ab62e3a7ff17a3384f2f0bca058fb957ade028ee
TheWookersquatch <ksprettygurl@gmail.com>
2021-07-13 11:40:39 -0600

    initial commit

---
+++ ORMDapperExercise1/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ORMDapperExercise1
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments();
+        
+    }
+}
--- ORMDapperExercise1/Program.cs
+++ ORMDapperExercise1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace ORMDapperExercise1
 {
@@ -6,7 +10,16 @@ namespace ORMDapperExercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            //var repo = new DepartmentRepository(connString);  //Without Dapper
+            IDbConnection conn = new MySqlConnection(connString);
+            var repo = new DapperDepartmentRepository(conn);
+
         }
     }
 }

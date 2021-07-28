a471f8291afebb9fd0c36d23908d17fb52baf04e
TheWookersquatch <ksprettygurl@gmail.com>
2021-07-28 11:01:39 -0600

    Dapper2 done

---
+++ ORMDapperExercise1/DapperProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using ORMDapperExercise1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Dapper;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public void CreateProduct(string newProductName, double newProductPrice, int newCategoryID)
+        {
+            _connection.Execute("INSERT INTO PRODUCTS (Name, Price, CategoryID) VALUES (@productName, @productPrice, @productCategoryID);", 
+                new { productName = newProductName, productPrice = newProductPrice, productCategoryID = newCategoryID });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM ProductS;");
+        }
+        
+    }
+}
--- ORMDapperExercise1/Program.cs
+++ ORMDapperExercise1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.IO;
+using BestBuyBestPractices;
 using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
 
@@ -16,10 +17,43 @@ namespace ORMDapperExercise1
                 .Build();
 
             string connString = config.GetConnectionString("DefaultConnection");
-            //var repo = new DepartmentRepository(connString);  //Without Dapper
+            //var repo = new DapperDepartmentRepository(connString);  //Without Dapper
             IDbConnection conn = new MySqlConnection(connString);
             var repo = new DapperDepartmentRepository(conn);
+            var productRepo = new DapperProductRepository(conn);
+            Console.WriteLine("Type a new Department name");
+            var newDepartment = Console.ReadLine();
+            repo.InsertDepartment(newDepartment);
+            var departments = repo.GetAllDepartments();
+            foreach (var dept in departments)
+            {
+                Console.WriteLine(dept.Name);
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("*************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("Type a new Product name");
+            var newProduct = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("How much does it cost?");
+            var newProductPrice = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("What is its CategoryID?");
+            var newCategoryID = int.Parse(Console.ReadLine());
+            productRepo.CreateProduct(newProduct, newProductPrice, newCategoryID);
+            var products = productRepo.GetAllProducts();
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Name}  {product.Price}   {product.CategoryID}");
+            }
+            //     Bonus:
+            // Create the UpdateProduct method in the DapperProductRepository class and implement in Program.cs
+            //
+            //     Extra Bonus:
+            // Create the DeleteProduct method
+            //
+            // HINT: you will need to delete records from the Sales table and the Reviews table where that Product may be referenced.You can do this all in the DeleteProduct method you are creating
         }
     }
 }
